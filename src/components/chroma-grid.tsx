78f4a1cb9c4d55971962eea970bc1984bc58199d
Aryan Vats <aryanvats220606@gmail.com>
2026-01-31 21:16:59 +0000

    in profile add glow effect in each logo box here is the effect install n

--- src/components/chroma-grid.tsx
+++ src/components/chroma-grid.tsx
@@ -167,6 +167,20 @@ const ChromaGrid: FC<ChromaGridProps> = ({ items, className = '', radius = 300, 
           } as React.CSSProperties}
         >
           <div
+            className="absolute w-[300%] h-[50%] opacity-0 group-hover:opacity-70 bottom-[-11px] right-[-250%] rounded-full animate-star-movement-bottom z-0"
+            style={{
+              background: `radial-gradient(circle, ${c.borderColor || 'white'}, transparent 10%)`,
+              animationDuration: '5s'
+            }}
+          ></div>
+          <div
+            className="absolute w-[300%] h-[50%] opacity-0 group-hover:opacity-70 top-[-10px] left-[-250%] rounded-full animate-star-movement-top z-0"
+            style={{
+              background: `radial-gradient(circle, ${c.borderColor || 'white'}, transparent 10%)`,
+              animationDuration: '5s'
+            }}
+          ></div>
+          <div
             className="absolute inset-0 pointer-events-none transition-opacity duration-500 z-20 opacity-0 group-hover:opacity-100"
             style={{
               background:
