7b8671dbba2ba075e4c6c74f6f0eb4dd4e867e46
Aryan Vats <aryanvats220606@gmail.com>
2026-02-07 15:46:37 +0000

    see the web page is divided int 4 sections, and i think the division of

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
         loop={false}
         className="text-4xl md:text-6xl font-headline text-center font-bold"
       />
-      <div className="inline-flex gap-4 mt-16">
+      <div className="inline-flex gap-4 mt-12">
         <Button asChild size="lg">
           <Link href="/portfolio">PORTFOLIO</Link>
         </Button>
@@ -36,7 +36,7 @@ export default function Home() {
           <Link href="/contact">CONTACT US</Link>
         </Button>
       </div>
-      <div className="w-full h-[680px] relative mt-16">
+      <div className="w-full h-[600px] relative mt-12">
         <CircularGallery items={galleryItems} bend={1} textColor="#ffffff" borderRadius={0.05} scrollEase={0.05} scrollSpeed={0.5} autoScrollSpeed={0.05} />
       </div>
     </div>
