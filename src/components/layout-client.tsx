f43e76bbb72d8f5d0d28e168160278aec2254e3c
Aryan Vats <aryanvats220606@gmail.com>
2026-02-11 22:17:00 +0000

    nope reverse the background

--- src/components/layout-client.tsx
+++ src/components/layout-client.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import { usePathname } from 'next/navigation';
@@ -12,24 +11,22 @@ export default function LayoutClient({ children }: { children: React.ReactNode }
   return (
     <>
       {showHeavyAnimations && <SplashCursor />}
-      {showHeavyAnimations && (
-        <div className="fixed top-0 left-0 -z-20 h-full w-full">
-          <LightPillar
-            topColor="#5227FF"
-            bottomColor="#FF9FFC"
-            intensity={0.7}
-            rotationSpeed={0.2}
-            glowAmount={0.002}
-            pillarWidth={5.5}
-            pillarHeight={0.4}
-            noiseIntensity={0.5}
-            pillarRotation={26}
-            interactive={false}
-            mixBlendMode="screen"
-            quality="low"
-          />
-        </div>
-      )}
+      <div className="fixed top-0 left-0 -z-20 h-full w-full">
+        <LightPillar
+          topColor="#5227FF"
+          bottomColor="#FF9FFC"
+          intensity={1}
+          rotationSpeed={0.3}
+          glowAmount={0.003}
+          pillarWidth={5.5}
+          pillarHeight={0.4}
+          noiseIntensity={0.5}
+          pillarRotation={26}
+          interactive={false}
+          mixBlendMode="screen"
+          quality="medium"
+        />
+      </div>
       {children}
     </>
   );
