8351d1b9013269e948fabf851c5292d709ad7654
Aryan Vats <aryanvats220606@gmail.com>
2026-02-21 14:18:36 +0000

    move game intro and shippy ad button just a little bit up, a little bit

--- src/components/AfterEffectsPortfolio.tsx
+++ src/components/AfterEffectsPortfolio.tsx
@@ -27,12 +27,12 @@ const projects = [
   {
     name: 'Shippy ad',
     videoUrl: 'https://drive.google.com/file/d/1PVgA1o4jivQbhwZl1w3RvJni2usDSfZF/view?usp=drive_link',
-    position: 'absolute top-[45%] left-16 z-10'
+    position: 'absolute top-[40%] left-16 z-10'
   },
   {
     name: 'Game Intro',
     videoUrl: 'https://drive.google.com/file/d/1evq4KHfP9D3KEL644ntheVwrnH1c4I-2/view?usp=drive_link',
-    position: 'absolute top-[45%] right-16 z-10'
+    position: 'absolute top-[50%] right-16 z-10'
   },
   {
     name: 'Stop Motion',
